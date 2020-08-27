e415d3063aee850080e4f0b24e1f474cc4a1037c
anton <uawesome13@gmail.com>
2020-08-27 09:44:05 +0300

    refactoring
    plus comments and docstrings added

--- tasks.py
+++ tasks.py
@@ -6,23 +6,52 @@ import numpy as np
 
 
 def fact(n: int) -> int:
+    """
+        Return n!
+    """
     return reduce(mul, range(1, n + 1))
 
 
-def digits_sum(number: int) -> int:
-    factorial = fact(number)
-    return reduce(lambda a, b: int(a) + int(b), str(factorial))
-
-
+# Task 1
 def number_of_true_brackets(N: int) -> int:
+    """
+        Return amount of balanced brackets expressions,
+        which can be made of N brackets pairs.
+        Actually return Nth element of Сatalan sequence.
+    """
     return int(1 / (N + 1) * (fact(2 * N) / fact(N) ** 2))
 
 
+# Task 2
 class Dijkstra:
-    def __init__(self, cities, cost_matrix, city_to_start_with):
+    """
+        To get instance of this class, put into __init__
+        parameters in following format:
+            cities = ['city1', 'city2', 'city3', ...]
+            cost_matrix = [[0, 3, inf],
+                           [3, 0, 1],
+                           [inf, 1, 0]]
+        Cities is just a full list of cites.
+        In cost matrix each element has index, appropriate to
+        city position into cities list. (i,j)th element is a price
+        of travel from i-th city to j-th or otherwise.
+        Note that matrix has to be symmetric, and has zeros
+        on the main diagonal.
+        Аfter creating an instance, it will store start_city, which
+        serves as a starting point for the distances to other cities.
+        You can change it by change_start_city(new_start_city).
+        After invoked fit(), you can get all distances to all cities,
+        starting from start_city in distances, which format is:
+        {'city1': 123.0, 'city2': 56.0, ...} or some special distance
+        by distance_to_city(finish_city).
+        So act in the following gradations:
+        get instance -> change start if necessary -> fit -> get distance
+    """
+
+    def __init__(self, cities, cost_matrix):
         self.cities = cities
         self.cost_matrix = np.array(cost_matrix)
-        self.city_to_start_with = city_to_start_with
+        self.start_city = self.cities[0]
         self.distances = {}
 
     def __repr__(self):
@@ -32,17 +61,28 @@ class Dijkstra:
             + "\ncost matrix:\n"
             + str(self.cost_matrix)
             + "\ncity to start with: "
-            + str(self.city_to_start_with)
+            + str(self.start_city)
             + "\ndistances: "
             + str(self.distances)
         )
 
-    def change_start_city(self, new_city_to_start_with):
-        self.city_to_start_with = new_city_to_start_with
+    def change_start_city(self, new_start_city):
+        """
+            Change start city.
+        """
+        self.start_city = new_start_city
 
     def fit(self):
-        index_start_city = self.cities.index(self.city_to_start_with)
+        """
+            This function is just implementation of Dijkstra's alghoritm.
+            If you have your start city changed, fit() will replace
+            elements in cost matrix and cities list, so that you will
+            have conformity between them.
+        """
+        index_start_city = self.cities.index(self.start_city)
+        cities_amount = len(self.cities)
 
+        # replace columns in cost matrix
         first_col = deepcopy(self.cost_matrix[:, 0])
         start_col = deepcopy(self.cost_matrix[:, index_start_city])
         self.cost_matrix[:, 0], self.cost_matrix[:, index_start_city] = (
@@ -50,6 +90,7 @@ class Dijkstra:
             first_col,
         )
 
+        # replace rows in cost matrix
         first_row = deepcopy(self.cost_matrix[0, :])
         start_row = deepcopy(self.cost_matrix[index_start_city, :])
         self.cost_matrix[0, :], self.cost_matrix[index_start_city, :] = (
@@ -57,25 +98,42 @@ class Dijkstra:
             first_row,
         )
 
-        self.cities.remove(self.city_to_start_with)
-        self.cities.insert(0, self.city_to_start_with)
+        # put new start city on the first place in cities list
+        self.cities.remove(self.start_city)
+        self.cities.insert(0, self.start_city)
 
-        for i in range(len(self.cities)):
+        # start of Dijkstra's alghoritm
+        # first get distances like {'city1':0,'city2': inf,'city3': inf, ...}
+        for i in range(cities_amount):
             distance = 0 if i == 0 else inf
             self.distances.update({self.cities[i]: distance})
 
+        # now lets move through all prices and take better of them
         for city in self.distances:
-            for i in range(len(self.cities)):
-                if (
-                    self.distances[city] + self.cost_matrix[self.cities.index(city), i]
-                    < list(self.distances.values())[i]
-                ):
+            for i in range(cities_amount):
+
+                current_city_weight = self.distances[city]
+                goal_city_price = self.cost_matrix[self.cities.index(city), i]
+                goal_city_name = list(self.distances.keys())[i]
+                goal_city_weight = list(self.distances.values())[i]
+
+                if current_city_weight + goal_city_price < goal_city_weight:
                     self.distances.update(
-                        {
-                            list(self.distances.keys())[i]: self.distances[city]
-                            + self.cost_matrix[self.cities.index(city), i]
-                        }
+                        {goal_city_name: current_city_weight + goal_city_price}
                     )
 
     def distance_to_city(self, finish_city):
+        """
+            Return distance from start city to finish city
+        """
         return self.distances[finish_city]
+
+
+# Task 3
+def digits_sum(number: int) -> int:
+    """
+        Takes some number and return sum of digits
+         of this number's factorial
+    """
+    factorial = fact(number)
+    return reduce(lambda a, b: int(a) + int(b), str(factorial))
--- tests.py
+++ tests.py
@@ -8,6 +8,33 @@ class TestSumOfFactDigits(unittest.TestCase):
         self.assertEqual(digits_sum(100), 648)
 
 
+class CheapestTravel(unittest.TestCase):
+    def test_first_case(self):
+        dij = Dijkstra(
+            ["gdansk", "bydgoszcz", "torun", "warszawa"],
+            [[0, 1, 3, inf],
+             [1, 0, 1, 4],
+             [3, 1, 0, 1],
+             [inf, 4, 1, 0]],
+        )
+
+        dij.fit()
+        self.assertEqual(dij.distance_to_city("warszawa"), 3)
+
+    def test_second_case(self):
+        dij = Dijkstra(
+            ["gdansk", "bydgoszcz", "torun", "warszawa"],
+            [[0, 1, 3, inf],
+             [1, 0, 1, 4],
+             [3, 1, 0, 1],
+             [inf, 4, 1, 0]],
+        )
+
+        dij.change_start_city('bydgoszcz')
+        dij.fit()
+        self.assertEqual(dij.distance_to_city("warszawa"), 2)
+
+
 class NumerOfTrueBrackets(unittest.TestCase):
     def test_N_1(self):
         self.assertEqual(number_of_true_brackets(1), 1)
@@ -23,34 +50,6 @@ class NumerOfTrueBrackets(unittest.TestCase):
 
     def test_N_7(self):
         self.assertEqual(number_of_true_brackets(7), 429)
-
-
-class CheapestTravel(unittest.TestCase):
-    def test_first_case(self):
-        dij = Dijkstra(
-            ["gdansk", "bydgoszcz", "torun", "warszawa"],
-            [[0, 1, 3, inf],
-             [1, 0, 1, 4],
-             [3, 1, 0, 1],
-             [inf, 4, 1, 0]],
-            "gdansk",
-        )
-
-        dij.fit()
-        self.assertEqual(dij.distance_to_city("warszawa"), 3)
-
-    def test_second_case(self):
-        dij = Dijkstra(
-            ["gdansk", "bydgoszcz", "torun", "warszawa"],
-            [[0, 1, 3, inf],
-             [1, 0, 1, 4],
-             [3, 1, 0, 1],
-             [inf, 4, 1, 0]],
-            "bydgoszcz",
-        )
-
-        dij.fit()
-        self.assertEqual(dij.distance_to_city("warszawa"), 2)
 
 
 if __name__ == "__main__":
